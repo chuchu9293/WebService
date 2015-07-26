9bbd7126bace1c81ae75202ef2e12d4a223ae934
yc_du <yc_du@Ctrip.com>
2015-07-26 16:07:03 +0800

    update

--- src/com/likeyichu/webservice/resource/Book.java
+++ src/com/likeyichu/webservice/resource/Book.java
@@ -5,9 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
 @Path("book")
+@JsonAutoDetect
+@JsonPropertyOrder(value = {"price", "name"})
+@JsonIgnoreProperties(value = {"year"})
 public class Book {
 	public String name = "Physics";
+	public String price = "123";
+	public String year = "2015";
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Book wsBook(){
